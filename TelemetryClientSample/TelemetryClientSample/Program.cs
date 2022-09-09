50f8c8656ce1521a1d759357b66f9aac280c0316
neha-bhargava <nebharg@microsoft.com>
2022-09-09 09:28:36 -0700

    Change to cca

--- TelemetryClientSample/TelemetryClientSample/Program.cs
+++ TelemetryClientSample/TelemetryClientSample/Program.cs
@@ -1,59 +1,127 @@
-﻿using Microsoft.Identity.Client;
-using Microsoft.IdentityModel.S2S.Telemetry;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-namespace TelemetryClientSample
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.S2S.Telemetry;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates; //Only import this if you are using certificate
+using System.Threading.Tasks;
+
+namespace daemon_console
 {
+    /// <summary>
+    /// This sample shows how to query the Microsoft Graph from a daemon application
+    /// which uses application permissions.
+    /// For more information see https://aka.ms/msal-net-client-credentials
+    /// </summary>
     class Program
     {
         private static string s_clientId = "1d18b3b0-251b-4714-a02a-9956cec86c2d";
+        private static string s_clientIdForConfidentialApp = "16dab2ba-145d-4b1b-8569-bf4b9aed4dc8";
+        private static Uri s_ccaAuthority = new Uri("https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47");
+        private static string s_confidentialClientSecret = "";
         private static readonly IEnumerable<string> s_scopes = new[] {
-            "user.read", "openid" };
+             $"{s_clientId}/.default" };
 
         static async Task Main(string[] args)
         {
             try
             {
-                var pca = CreateApp();
-
-                Console.WriteLine("====Acquire token interactively====");
-                var result = await pca.AcquireTokenInteractive(s_scopes)
-                .ExecuteAsync().ConfigureAwait(false);
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine("Access Token = " + result?.AccessToken);
-                Console.ResetColor();
-
-                Console.WriteLine("=====Acquire token silently======");
-                var account = (await pca.GetAccountsAsync().ConfigureAwait(false)).Single();
-
-                result = await pca.AcquireTokenSilent(s_scopes, account).ExecuteAsync().ConfigureAwait(false);
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine("Access Token = " + result?.AccessToken);
-                Console.ResetColor();
+                RunAsync().GetAwaiter().GetResult();
             }
-            catch (MsalException e)
+            catch (Exception ex)
             {
-                Console.BackgroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Error: ErrorCode=" + e.ErrorCode + "ErrorMessage=" + e.Message);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
                 Console.ResetColor();
             }
 
-            Console.Read();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
 
-        private static IPublicClientApplication CreateApp()
+        private static async Task RunAsync()
         {
-            return PublicClientApplicationBuilder.Create(s_clientId)
-                 .WithTenantId("72f988bf-86f1-41af-91ab-2d7cd011db47")
-                 .WithExperimentalFeatures()
-                 .WithDefaultRedirectUri()
-                 .WithLogging(MyLoggingMethod, LogLevel.Info, true, false)
-                 .WithTelemetryClient(new AriaTelemetryClient(s_clientId))
-                 .Build();
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+
+            // Even if this is a console application here, a daemon application is a confidential client application
+            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .WithExperimentalFeatures()
+                .WithTelemetryClient(new AriaTelemetryClient(config.ClientId))
+                .Build();
+
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator. 
+            string[] scopes = new string[] { $"{config.ApiUrl}.default" };
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                // Mitigation: change the scope to be as expected
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Scope provided is not supported");
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                await apiCaller.CallWebApiAndProcessResultASync($"{config.ApiUrl}v1.0/users", result.AccessToken, Display);
+            }
         }
 
-        static void MyLoggingMethod(LogLevel level, string message, bool containsPii)
+        /// <summary>
+        /// Display the result of the Web API call
+        /// </summary>
+        /// <param name="result">Object to display</param>
+        private static void Display(JObject result)
         {
-            Console.WriteLine($"MSALTest {level} {containsPii} {message}");
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            {
+                Console.WriteLine($"{child.Name} = {child.Value}");
+            }
         }
+
+        /// <summary>
+        /// Checks if the sample is configured for using ClientSecret or Certificate. This method is just for the sake of this sample.
+        /// You won't need this verification in your production application since you will be authenticating in AAD using one mechanism only.
+        /// </summary>
+        /// <param name="config">Configuration from appsettings.json</param>
+        /// <returns></returns>
+        private static bool AppUsesClientSecret(AuthenticationConfig config)
+        {
+            string clientSecretPlaceholderValue = "[Enter here a client secret for your application]";
+            string certificatePlaceholderValue = "[Or instead of client secret: Enter here the name of a certificate (from the user cert store) as registered with your application]";
+
+            if (!String.IsNullOrWhiteSpace(config.ClientSecret) && config.ClientSecret != clientSecretPlaceholderValue)
+            {
+                return true;
+            }
+
+            else if (!String.IsNullOrWhiteSpace(config.CertificateName) && config.CertificateName != certificatePlaceholderValue)
+            {
+                return false;
+            }
+
+            else
+                throw new Exception("You must choose between using client secret or certificate. Please update appsettings.json file.");
+        }
+
     }
 }
